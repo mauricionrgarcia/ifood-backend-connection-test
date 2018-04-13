6fa74cac69634e7c1b0aad8657104df6bf7b29ef
mushira4 <mushira4@gmail.com>
2018-04-13 02:12:42 -0300

    Adding HealthCheckCurrentStatusFeatureTest acceptance test

---
+++ interface/src/acceptanceTest/java/com/ifood/HealthCheckCurrentStatusFeatureTest.java
@@ -0,0 +1,54 @@
+package com.ifood;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class HealthCheckCurrentStatusFeatureTest {
+
+    @Test
+    public void shouldReturnIfARestaurantIsCurrentlyOnline() {
+        String restaurantCode = "restaurant1";
+        sendHealthSignal(restaurantCode);
+        checkIfOnline(restaurantCode);
+    }
+
+    private void checkIfOnline(String restaurantCode) {
+        Response post = given()
+                .content("[ \"" + restaurantCode + "\"]")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/connection/health/online/list");
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        if(localDateTime .getHour() < 9 || localDateTime.getHour() == 23){
+             post.then()
+                    .body("[0].online", equalTo(Boolean.FALSE)) //TODO: Need to find a way to the test time be controllable
+                    .statusCode(200);
+        } else {
+            post.then()
+                    .body("[0].online", equalTo(Boolean.TRUE)) //TODO: Need to find a way to the test time be controllable
+                    .statusCode(200);
+        }
+    }
+
+    private void sendHealthSignal(String restaurantCode) {
+        when()
+                .post("/connection/health/{restaurant_code}", restaurantCode)
+                .then()
+                .statusCode(200);
+    }
+
+}
